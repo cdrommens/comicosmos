fb8d767b0cc5e27dc95525467b84e8c3ca3098a4
Cederik Rommens <cederik.rommens@kbc.be>
2020-03-29 10:36:47 +0200

    COSMOS-17 : setup multi module maven project

--- ZEUS-comic-service/src/main/java/be/rommens/zeus/ComicosmosApplication.java
+++ ZEUS-comic-service/src/main/java/be/rommens/zeus/ComicosmosApplication.java
@@ -1,4 +1,4 @@
-package be.rommens.comicosmos;
+package be.rommens.zeus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ZEUS-comic-service/src/test/java/be/rommens/zeus/ComicosmosApplicationTests.java
+++ ZEUS-comic-service/src/test/java/be/rommens/zeus/ComicosmosApplicationTests.java
@@ -1,11 +1,9 @@
-package be.rommens.comicosmos;
+package be.rommens.zeus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
