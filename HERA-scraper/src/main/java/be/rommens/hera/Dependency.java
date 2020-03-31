0185d0fdac277fa2930bb942c8870937fbe48578
Cederik Rommens <cederik.rommens@kbc.be>
2020-03-31 15:16:55 +0200

    COSMOS-17 : transform the library into a spring project

--- HERA-scraper/src/main/java/be/rommens/hera/Dependency.java
+++ HERA-scraper/src/main/java/be/rommens/hera/Dependency.java
@@ -1,18 +1,35 @@
 package be.rommens.hera;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Service;
+
 /**
  * User : cederik
  * Date : 29/03/2020
  * Time : 13:32
  */
+@Service
+@EnableConfigurationProperties(Property.class)
 public class Dependency {
+
+    private final Property property;
     private String name;
 
-    public Dependency(String name) {
+    public Dependency(Property property) {
+        this.property = property;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
     public String hello() {
-        return "dependency " + name;
+        return "dependency " + getName() + " " + property.getMessage();
     }
 }
