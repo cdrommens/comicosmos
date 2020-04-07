5e57fce320e916536f9266c182306db184efec1d
cdrommens <62701814+cdrommens@users.noreply.github.com>
2020-04-07 15:32:18 +0200

    Update ScraperAutoConfigurationTest.java

--- HERA-scraper-spring-boot-autoconfigure/src/test/java/be/rommens/hera/autoconfigure/ScraperAutoConfigurationTest.java
+++ HERA-scraper-spring-boot-autoconfigure/src/test/java/be/rommens/hera/autoconfigure/ScraperAutoConfigurationTest.java
@@ -4,7 +4,7 @@ import be.rommens.hera.api.Provider;
 import be.rommens.hera.api.service.ScraperFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -15,7 +15,7 @@ import static org.hamcrest.Matchers.notNullValue;
  * Date : 05/04/2020
  * Time : 09:43
  */
-@SpringJUnitConfig(classes = ScraperAutoConfiguration.class)
+@SpringBootTest(classes = ScraperAutoConfiguration.class)
 public class ScraperAutoConfigurationTest {
 
     @Autowired
