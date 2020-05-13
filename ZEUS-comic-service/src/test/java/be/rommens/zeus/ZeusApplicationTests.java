f6310df5dd28a3a24c52a5d6c8bd32c30df5d1d7
Cederik Rommens <cederik.rommens@kbc.be>
2020-05-13 16:07:15 +0200

    COSMOS-66 : try increase code coverage

--- ZEUS-comic-service/src/test/java/be/rommens/zeus/ZeusApplicationTests.java
+++ ZEUS-comic-service/src/test/java/be/rommens/zeus/ZeusApplicationTests.java
@@ -1,13 +1,12 @@
 package be.rommens.zeus;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ZeusApplicationTests {
 
     @Test
     void contextLoads() {
+        ZeusApplication.main(new String[] {});
     }
 
 }
